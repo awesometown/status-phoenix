62ae694ddd698485528c21db2d54acdc3b7dbae6
John Price <joprice@ea.com>
2016-08-18 14:18:16 -0400

    Styling on login screen.

--- web/static/js/views/sessions/new.js
+++ web/static/js/views/sessions/new.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { Grid, Row, Col, Panel, Button } from "react-bootstrap";
 
 import { setDocumentTitle } from '../../utils';
 import Actions from '../../actions/sessions';
@@ -31,40 +32,44 @@ class SessionsNew extends React.Component {
 		);
 	}
 
-render() {
-    return (
-      <div className='view-container sessions new'>
-        <main>
-          <header>
-            <div className="logo" />
-          </header>
-          <form id="sign_in_form" onSubmit={::this._handleSubmit}>
-            {::this._renderError()}
-            <div className="field">
-              <input
-                ref="email"
-                type="Email"
-                id="user_email"
-                placeholder="Email"
-                required="true"
-                defaultValue="test@example.com"/>
-            </div>
-            <div className="field">
-              <input
-                ref="password"
-                type="password"
-                id="user_password"
-                placeholder="Password"
-                required="true"
-                defaultValue="asdfg"/>
-            </div>
-            <button type="submit">Sign in</button>
-          </form>
-          <Link to="/sign_up">Create new account</Link>
-        </main>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Grid>
+				<Row>
+					<Col mdOffset={4} md={4}>
+						<Panel className="text-center">
+							<form id="sign_in_form" role="form" onSubmit={::this._handleSubmit}>
+								{::this._renderError()}
+								<div className="form-group">
+									<label>
+										<input className="form-control text-center"
+										       ref="email"
+										       type="Email"
+										       id="user_email"
+										       placeholder="Email"
+										       required="true"
+										       defaultValue="test@example.com"/>
+									</label>
+								</div>
+								<div className="form-group">
+									<label>
+										<input ref="password"
+										       type="password"
+										       className="form-control text-center"
+										       id="user_password"
+										       placeholder="Password"
+										       required="true"
+										       defaultValue="asdfg"/>
+									</label>
+								</div>
+								<Button bsStyle="success" onClick={::this._handleSubmit}>Login</Button>
+							</form>
+						</Panel>
+					</Col>
+				</Row>
+			</Grid>
+		);
+	}
 }
 
 const mapStateToProps = (state) => (
