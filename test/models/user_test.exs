8f0ec4841e610a3ef26277100063c9660d297407
John Price <john.price@gmail.com>
2016-08-18 23:35:39 -0400

    Fix failing test.

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule StatusPhoenix.UserTest do
 
   alias StatusPhoenix.User
 
-  @valid_attrs %{email: "some content", encrypted_password: "some content", first_name: "some content", last_name: "some content"}
+  @valid_attrs %{email: "foo@example.com", password: "some content", first_name: "some content", last_name: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
