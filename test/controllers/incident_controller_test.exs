30233c5b32327193bcc1b0e3193d49e298304b31
John Price <john.price@gmail.com>
2016-08-21 22:17:05 -0400

    Fixing more tests.

--- test/controllers/incident_controller_test.exs
+++ test/controllers/incident_controller_test.exs
@@ -17,7 +17,7 @@ defmodule StatusPhoenix.IncidentControllerTest do
   test "shows chosen resource", %{conn: conn} do
     incident = Repo.insert! %Incident{}
     conn = get conn, incident_path(conn, :show, incident)
-    assert json_response(conn, 200)["data"] == %{"id" => incident.id,
+    assert json_response(conn, 200) == %{"id" => incident.id,
       "title" => incident.title,
       "state" => incident.state,
       "type" => incident.type,
@@ -31,26 +31,26 @@ defmodule StatusPhoenix.IncidentControllerTest do
   end
 
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, incident_path(conn, :create), incident: @valid_attrs
-    assert json_response(conn, 201)["data"]["id"]
+    conn = post conn, incident_path(conn, :create), @valid_attrs
+    assert json_response(conn, 201)["id"]
     assert Repo.get_by(Incident, @valid_attrs)
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, incident_path(conn, :create), incident: @invalid_attrs
+    conn = post conn, incident_path(conn, :create), @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "updates and renders chosen resource when data is valid", %{conn: conn} do
     incident = Repo.insert! %Incident{}
-    conn = put conn, incident_path(conn, :update, incident), incident: @valid_attrs
-    assert json_response(conn, 200)["data"]["id"]
+    conn = put conn, incident_path(conn, :update, incident), @valid_attrs
+    assert json_response(conn, 200)["id"]
     assert Repo.get_by(Incident, @valid_attrs)
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
     incident = Repo.insert! %Incident{}
-    conn = put conn, incident_path(conn, :update, incident), incident: @invalid_attrs
+    conn = put conn, incident_path(conn, :update, incident), @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
